680709aaf37ffe19ab953c867a208c32665c3817
mjedral <miazga198@o2.pl>
2024-09-09 23:12:35 +0200

    added IComet interface and generatePrompt function

--- src/AILendingAggregator.sol
+++ src/AILendingAggregator.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "./Prompt.sol";
 import "./interfaces/IPoolDataProvider.sol";
+import "./interfaces/IComet.sol";
 
 contract AILendingAggregator {
     enum LendingPlatform {
@@ -16,16 +17,23 @@ contract AILendingAggregator {
     address public owner;
     Prompt public promptContract;
     IPoolDataProvider public aaveDataProvider;
+    IComet public comet;
+    address constant WETH_SEPOLIA = 0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
 
-    constructor(address _promptContractAddress, address _aaveDataProvider) {
+    constructor(
+        address _promptContractAddress,
+        address _aaveDataProvider,
+        address _comet
+    ) {
         owner = msg.sender;
         promptContract = Prompt(_promptContractAddress);
-        aaveDataProvider = IPoolDataProvider(_aaveDataProvider);
+        aaveDataProvider = IPoolDataProvider(_aaveDataProvider); // poolDataProvider on sepolia = 0x3e9708d80f7B3e43118013075F7e95CE3AB31F31
+        comet = IComet(_comet); // cWETHv3 = 0x2943ac1216979aD8dB76D9147F64E61adc126e96
     }
 
     // function to get AI result from the Prompt contract
@@ -34,6 +42,64 @@ contract AILendingAggregator {
         string calldata prompt
     ) external onlyOwner {
         AIResult = promptContract.getAIResult(modelId, prompt);
+    }
+
+    function generatePrompt() private returns (string memory) {
+        (
+            ,
+            ,
+            uint256 totalAToken,
+            uint256 totalStableDebt,
+            uint256 totalVariableDebt,
+            uint256 liquidityRate,
+            uint256 variableBorrowRate,
+            uint256 stableBorrowRate,
+            uint256 averageStableBorrowRate,
+            ,
+            ,
+
+        ) = aaveDataProvider.getReserveData(WETH_SEPOLIA); // add asset address
+
+        uint256 totalSupply = comet.totalSupply();
+        uint256 totalBorrow = comet.totalBorrow();
+        uint256 utilization = comet.getUtilization();
+        uint256 supplyRate = comet.getSupplyRate(utilization);
+        uint256 borrowRate = comet.getBorrowRate(utilization);
+
+        return
+            string(
+                abi.encodePacked(
+                    "I want to forecast the supply rate changes in the Aave and Compound protocol based on the following data. Please provide a prediction for the next 3 days. In both case we compare indicators for WETH token",
+                    "AAVE:",
+                    " Total Liquidity: ",
+                    totalAToken,
+                    ", Total Stable Debt: ",
+                    totalStableDebt,
+                    ", Total Variable Debt: ",
+                    totalVariableDebt,
+                    ", Liquidity Rate: ",
+                    liquidityRate,
+                    ", Variable Borrow Rate: ",
+                    variableBorrowRate,
+                    ", Stable Borrow Rate: ",
+                    stableBorrowRate,
+                    ", Average Stable Borrow Rate: ",
+                    averageStableBorrowRate,
+                    "_____________",
+                    ". COMPOUND: ",
+                    "Total Supply: ",
+                    totalSupply,
+                    ", Total Borrow: ",
+                    totalBorrow,
+                    ", Utilization: ",
+                    utilization,
+                    ", Supply Rate: ",
+                    supplyRate,
+                    ", Borrow Rate: ",
+                    borrowRate,
+                    "Based on these data, please provide an estimate of the future supply rate over the next 3 days and generate prompt only with result which option is better. Please answer only with name of protocol. I mean AAVE or COMPOUND"
+                )
+            );
     }
 
     // use calculateAIResult to generate prompt like:
